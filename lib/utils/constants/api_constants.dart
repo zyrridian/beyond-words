5c40ae16414705b577009bad6baff16964ff0584
Zyrridian <rezkyaf246@gmail.com>
2024-05-18 08:16:54 +0700

    delete unnecessary files and add commented code in case we need to use bottom app bar

--- lib/utils/constants/api_constants.dart
+++
@@ -1,8 +0,0 @@
-
-/* -- List of Constants used in APIs -- */
-
-// Example
-// const String tSecretAPIKey = "owt_dkhflahdflhadksjhfkjahfhjsa";
-class APIConstants {
-  static const String tSecretAPIKey = "owt_dkhflahdflhadksjhfkjahfhjsa";
-}
--- lib/utils/constants/enums.dart
+++
@@ -1,7 +0,0 @@
-// List of Enums, they cannot be created inside a class.
-
-enum TextSizes { small, medium, large }
-
-enum OrderStatus { processing, shipped, delivered }
-
-enum PaymentMethods { paypal, googlePay, applePay, visa, masterCard, creditCard, payStack, razorPay, paytm }
